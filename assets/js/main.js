8cdad378fa92365610d62cc4d92d74b9c9335107
scottsfarley93 <scott@firesphere.org>
2016-05-19 16:20:55 -0500

    Path

--- assets/js/main.js
+++ assets/js/main.js
@@ -161,6 +161,7 @@ var geojsonMarkerOptions = {
 		fName = name.replace(" ", "_")
     fName = fName.toUpperCase()
 		fName = fName + ".geojson"
+    console.log(FName);
 		file = "/PaleoView/assets/data/geojson/" + fName
 		//file = globals.defaultGeojson
 		$.ajax(file, {
